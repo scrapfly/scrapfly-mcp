f33147818436edb6d5323db3c327f671dec0e58f
Coling Brigato <colin@scrapfly.io>
2026-01-13 14:31:22 +0000

    Revert "Merge pull request #6 from scrapfly/ping_endpoint"
    This reverts commit 105a7926cb969dba4c980c5d76d2607ca10a40d3, reversing
    changes made to e2d757e840df3f637eca5a25ec89ae476939e44f.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -21,13 +21,7 @@ type StreamableServerFunction func(mcpHandler *mcp.StreamableHTTPHandler, httpAd
 type StdioServerFunction func(server *mcp.Server, t *mcp.LoggingTransport)
 
 func DefaultStreamableServerFunction(mcpHandler *mcp.StreamableHTTPHandler, httpAddr *string) {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
-	mux.Handle("/", mcpHandler)
-	log.Fatal(http.ListenAndServe(*httpAddr, mux))
+	log.Fatal(http.ListenAndServe(*httpAddr, mcpHandler))
 }
 
 func DefaultStdioServerFunction(server *mcp.Server, t *mcp.LoggingTransport) {
